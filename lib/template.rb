bba42aa55da957fc9e0c557492b77abb36d8262f
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-04-24 16:02:36 -0700

    Updating template to include additional steps

--- lib/template.rb
+++ lib/template.rb
@@ -3,12 +3,17 @@ require 'rake'
 require 'byebug'
 namespace = ENV['NAMESPACE']
 
+def original_wd
+  return @original_wd if @original_wd
+  raise RuntimeError.new("Doh! Rails changed an instance variable. We really need this for determining where we installed the gem.")
+end
+
 # Commit changes up to this point
 
-plugin_path = File.join(@original_wd,app_path)
+plugin_path = File.join(original_wd,name)
 inside plugin_path do
 
-  dirnames = Rake::FileList.new("app/**/#{app_path}", "lib/**/#{app_path}")
+  dirnames = Rake::FileList.new("app/**/#{name}", "lib/**/#{name}")
   dirnames.each do |dirname|
     target_dirname = dirname.sub(/\/#{namespace}_/, "/#{namespace}/")
     run "mkdir -p #{File.dirname(target_dirname)}"
@@ -28,5 +33,24 @@ inside plugin_path do
   # Add `s.license = "APACHE"` to gemspec
 
   # Add bundler style s.files and s.bin
+
+  # Move lib/namspaced_plugin.rb to lib/namspaced-plugin.rb
+
+  # For all files (Rake::FileList will be helpful) replace the text:
+  # `namespaced_plugin` with `namespaced-plugin`
+  # https://github.com/jeremyf/orcid/blob/master/script/fast_specs
+
+  # Create lib/namespaced_plugin.rb
+  # with `require "namespaced-plugin.rb"`
+
+  # For all files (Rake::FileList will be helpful) replace:
+  # `module NamespacedPlugin` with `module Namespaced\n  module Plugin`
+  # and add the corresponding closing `end` at EOF
+  # Hints from:
+  # https://github.com/jeremyf/orcid/blob/master/script/fast_specs
+
+  # For all files (Rake::FileList will be helpful) replace:
+  # `NamespacedPlugin` with `Namespaced::Plugin`
+  # https://github.com/jeremyf/orcid/blob/master/script/fast_specs
 
 end
